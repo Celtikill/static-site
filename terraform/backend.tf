7c65e440c4d552ccd1df6f5508cca3657fe7311e
Celtikill <celtikill@celtikill.io>
2025-07-10 13:16:49 -0400

    update backend config

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   backend "s3" {
     # Configuration provided via:
     # tofu init -backend-config=backend-dev.hcl
-    
+
     # All backend configuration is externalized to backend-dev.hcl
     # for security and environment-specific values
   }
