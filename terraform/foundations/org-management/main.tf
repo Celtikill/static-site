ef247643b6fa95ffa74ab9c0789666d7ec38faed
celtikill <celtikill@celtikill.io>
2025-09-08 11:16:18 -0400

    formatting in org-mgmnt module

--- terraform/foundations/org-management/main.tf
+++ terraform/foundations/org-management/main.tf
@@ -42,9 +42,9 @@ resource "aws_organizations_organization" "main" {
     "cloudtrail.amazonaws.com",
     "config.amazonaws.com"
   ]
-  
+
   feature_set = "ALL"
-  
+
   enabled_policy_types = [
     "SERVICE_CONTROL_POLICY"
   ]
