424aaf70c45079dd96b790d53d68cf9634ef89bc
celtikill <celtikill@celtikill.io>
2025-10-06 13:03:33 -0400

    url fixes

--- terraform/workloads/static-site/outputs.tf
+++ terraform/workloads/static-site/outputs.tf
@@ -155,7 +155,7 @@ output "website_url" {
   description = "Primary website URL"
   value = length(var.domain_aliases) > 0 ? (
     var.acm_certificate_arn != null ? "https://${var.domain_aliases[0]}" : "http://${var.domain_aliases[0]}"
-  ) : var.enable_cloudfront ? "https://${module.cloudfront[0].distribution_domain_name}" : (
+    ) : var.enable_cloudfront ? "https://${module.cloudfront[0].distribution_domain_name}" : (
     module.s3.website_endpoint != null ? "http://${module.s3.website_endpoint}" : null
   )
 }
